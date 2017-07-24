12fc1e84bec02933ea3f5b6628831325f60c6913
张新罡 <zhangxingang@yupiao.com>
2017-07-24 10:12:37 +0800

    update

--- handler/bloghandler.go
+++ handler/bloghandler.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"html"
+	"io/ioutil"
 	"net/http"
 	"zweb/dao"
 )
@@ -14,9 +15,16 @@ func GetBlogHandler(w http.ResponseWriter, r *http.Request) {
 	info, _ := json.Marshal(blog)
 	fmt.Println(r.RequestURI, r.Method, blog)
 	fmt.Fprintf(w, "Hello, %q,%v", html.EscapeString(r.URL.Path), string(info))
-	fmt.Println("get")
 }
 
 func PostBlogHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("post")
+	data, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		fmt.Println(err)
+	}
+	blog := dao.Blog{}
+	json.Unmarshal(data, &blog)
+	dao.AddBlog(blog)
+	w.Write([]byte("asdsd"))
 }
